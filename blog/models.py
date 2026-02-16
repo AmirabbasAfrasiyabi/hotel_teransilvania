16197d0fc2cd57238d9d9f60d20a5a26d56d55e1
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2026-02-16 10:58:07 +0330

    created nodels

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    # image
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #tag
+    #category
+    #author
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True , blank=True )
+    updated_date = models.DateTimeField(auto_now=True , blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
